39b7e39ef16dabf6b86d7f2436c6efce63f371f6
lwd <912915121@qq.com>
2019-12-06 13:17:54 +0800

    知乎爬虫代码

--- zhihu/zhihu_spider.py
+++ zhihu/zhihu_spider.py
@@ -12,18 +12,18 @@ import requests
 
 # 保存内容到本地
 def save_content(title, author_name, author_url, image_urls, request_header):
-    save_path = title + '/' + author_name
+    save_path = title
     if not os.path.exists(save_path):  # 创建保存路径文件
         os.makedirs(save_path)
 
     # 保存用户主页url
-    with open(save_path + '/author_url.txt', 'w', encoding='gb18030', errors='ignore') as f:
-        f.writelines(author_url)
+    with open(save_path + '/AAA-用户主页url.txt', 'a+', encoding='utf-8', errors='ignore') as f:
+        f.write(author_name + ":    " + author_url + '\n')
 
     # 开始下载用户图片
     percentage, total = 1, len(image_urls)
     for img_url in image_urls:
-        filename = save_path + '/' + str(random.randint(1000000, 9999999)) + ".jpg"
+        filename = save_path + '/' + author_name + '-' + str(random.randint(1000000, 9999999)) + ".jpg"
         print('正在下载：', author_name, '的图片，进度：', percentage, '/', total)
         urllib.request.urlretrieve(img_url, filename=filename)
         percentage = percentage + 1
@@ -67,7 +67,6 @@ if __name__ == '__main__':
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3941.4 Safari/537.36',
     }
     # 将url替换成你想要下载的问题的json链接
-    # url = 'https://www.zhihu.com/api/v4/questions/51310981/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=3&offset=3&platform=desktop&sort_by=default'
     url = 'https://www.zhihu.com/api/v4/questions/312744244/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B*%5D.topics&offset=0&limit=20&sort_by=updated'
     # get_answer_url(url, header)
 
